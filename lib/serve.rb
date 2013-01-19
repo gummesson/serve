bcc0a14037d8af506ee5124a9b4aec17ce3a38f3
Ellen Gummesson <ellengummesson@gmail.com>
2013-01-19 12:46:37 +0100

    Merged serve.rb and init.rb.

--- lib/serve.rb
+++ lib/serve.rb
@@ -5,7 +5,17 @@ require 'kramdown'
 require 'fileutils'
 
 # App Library
-require 'serve/init'
 require 'serve/build'
 require 'serve/read'
 require 'serve/parse'
+
+module Serve
+  class Init
+    # Run Serve
+    def self.run(content_dir, template_dir, static_dir)
+      Build.process(content_dir, template_dir, static_dir)
+      Build.transfer(template_dir, static_dir)
+      Build.done(static_dir)
+    end
+  end
+end
