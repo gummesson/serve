677dc80014328044a9c1881e470b42755c340f63
Ellen Gummesson <ellengummesson@gmail.com>
2013-08-31 20:28:32 +0200

    Cleanup.

--- bin/serve.rb
+++ bin/serve.rb
@@ -14,29 +14,23 @@ require 'serve'
 #  Default: kramdown
 #   Others: redcarpet, rdiscount or maruku
 #
-options = {
-  :content  => "content",
-  :template => "template",
-  :static   => "static",
-  :parser   => "kramdown"
-}
+options = { :content  => "content",
+            :template => "template",
+            :static   => "static",
+            :parser   => "kramdown" }
 
 # Options
 OptionParser.new do |opt|
   opt.banner = "Usage: serve [options]"
-
   opt.on("-c", "--content DIRECTORY", "Content directory") do |c|
     options[:content] = c
   end
-
   opt.on("-t", "--template DIRECTORY", "Template directory") do |t|
     options[:template] = t
   end
-
   opt.on("-s", "--static DIRECTORY", "Static directory") do |s|
     options[:static] = s
   end
-
   opt.on("-p", "--parser GEM", "kramdown, redcarpet, rdiscount or maruku") do |p|
     options[:parser] = p
   end
--- lib/serve/parse.rb
+++ lib/serve/parse.rb
@@ -26,24 +26,19 @@ module Serve
       when "kramdown"
         require 'kramdown'
         return Kramdown::Document.new(content, :auto_ids => false).to_html
-
       when "redcarpet"
         require 'redcarpet'
         render_html    = Redcarpet::Render::HTML
         render_options = { :no_intra_emphasis  => true,
                            :fenced_code_blocks => true }
-
         redcarpet = Redcarpet::Markdown.new(render_html, render_options)
         return redcarpet.render(content)
-
       when "rdiscount"
         require 'rdiscount'
         return RDiscount.new(content).to_html
-
       when "maruku"
         require 'maruku'
         return Maruku.new(content).to_html_document
-
       else
         raise "Please choose a valid Markdown parser. For more information, see 'serve --help'."
       end
